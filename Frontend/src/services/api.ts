5e39ade0dd1ce7cae1ba155dd80126bda3ef0a98
nightkai27200 <kevdev59177@gmail.com>
2026-07-26 20:10:55 +0200

    Connect frontend to Railway backend

--- Frontend/src/services/api.ts
+++ Frontend/src/services/api.ts
@@ -4,7 +4,7 @@ import axios from "axios";
 const api = axios.create({
 
   baseURL:
-    "http://localhost:3001",
+   "https://factureco-production.up.railway.app",
 
   headers:{
     "Content-Type":"application/json",
