bab1e926a3cf1e3ae7e2d1bef9c88d16b7198554
nightkai27200 <kevdev59177@gmail.com>
2026-07-27 11:38:50 +0200

    allow vercel cors and railway port

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,38 +2,34 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 
-
 async function bootstrap() {
 
-
-  const app =
-    await NestFactory.create(
-      AppModule,
-      {
-        rawBody:true
-      }
-    );
-
+  const app = await NestFactory.create(
+    AppModule,
+    {
+      rawBody: true
+    }
+  );
 
 
   app.enableCors({
 
-    origin:[
+    origin: [
+      "https://factureco.vercel.app",
       "http://localhost:3001",
       "http://localhost:3000",
       "http://192.168.1.43:3001",
     ],
 
-    credentials:true,
+    credentials: true,
 
   });
 
 
-
-  await app.listen(3000);
-
+  await app.listen(
+    process.env.PORT || 3000
+  );
 
 }
-
 
 bootstrap();
