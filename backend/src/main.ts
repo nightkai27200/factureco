238c5a1f0526c767c96b32f583a52cf8260be32d
nightkai27200 <kevdev59177@gmail.com>
2026-07-27 14:43:26 +0200

    add new vercel cors domain

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -9,26 +9,15 @@ async function bootstrap() {
 
 
   app.enableCors({
-    origin: [
-      "https://factureco.vercel.app",
-      "https://factureco-e24f74xd7-ms-consulting.vercel.app",
-      "http://localhost:3000",
-      "http://localhost:3001",
-    ],
-    credentials: true,
-    methods: [
-      "GET",
-      "POST",
-      "PUT",
-      "PATCH",
-      "DELETE",
-      "OPTIONS",
-    ],
-    allowedHeaders: [
-      "Content-Type",
-      "Authorization",
-    ],
-  });
+  origin: [
+    "https://factureco.vercel.app",
+    "https://factureco-e24f74xd7-ms-consulting.vercel.app",
+    "https://factureco-ldcvitf5s-ms-consulting.vercel.app",
+    "http://localhost:3000",
+    "http://localhost:3001",
+  ],
+  credentials: true,
+});
 
 
   await app.listen(process.env.PORT || 3000);
