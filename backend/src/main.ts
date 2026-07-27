6242cfe592e472e0eb8a51fbbacde067a4ff27a1
nightkai27200 <kevdev59177@gmail.com>
2026-07-27 14:03:00 +0200

    fix cors origin vercel

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,38 +1,37 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-
 async function bootstrap() {
 
-  const app = await NestFactory.create(
-    AppModule,
-    {
-      rawBody: true,
-    }
-  );
+  const app = await NestFactory.create(AppModule, {
+    rawBody: true,
+  });
 
 
   app.enableCors({
-  origin: true,
-  credentials: true,
-  methods: [
-    "GET",
-    "POST",
-    "PUT",
-    "PATCH",
-    "DELETE",
-    "OPTIONS"
-  ],
-  allowedHeaders: [
-    "Content-Type",
-    "Authorization"
-  ],
-});
+    origin: [
+      "https://factureco.vercel.app",
+      "https://factureco-e24f74xd7-ms-consulting.vercel.app",
+      "http://localhost:3000",
+      "http://localhost:3001",
+    ],
+    credentials: true,
+    methods: [
+      "GET",
+      "POST",
+      "PUT",
+      "PATCH",
+      "DELETE",
+      "OPTIONS",
+    ],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+    ],
+  });
 
 
-  await app.listen(
-    process.env.PORT || 3000
-  );
+  await app.listen(process.env.PORT || 3000);
 
 }
 
