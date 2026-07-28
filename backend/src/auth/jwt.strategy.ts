0d6c597a22bd075f3b2e3bff4db06d57f793451c
nightkai27200 <kevdev59177@gmail.com>
2026-07-28 12:44:39 +0200

    Add JWT verify secret debug

--- backend/src/auth/jwt.strategy.ts
+++ backend/src/auth/jwt.strategy.ts
@@ -7,33 +7,24 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
 
   constructor() {
 
-    const jwtSecret = process.env.JWT_SECRET;
+  const jwtSecret = process.env.JWT_SECRET;
 
+  console.log("========================");
+  console.log("JWT STRATEGY START");
+  console.log("JWT SECRET LENGTH:", jwtSecret?.length);
+  console.log("JWT SECRET START:", jwtSecret?.substring(0,5));
+  console.log("========================");
 
-    console.log("========================");
-    console.log("JWT VERIFY SECRET LENGTH:", jwtSecret?.length);
-    console.log("JWT VERIFY SECRET:", jwtSecret);
-    console.log("========================");
-
-
-    if (!jwtSecret) {
-      throw new Error(
-        "JWT_SECRET manquant"
-      );
-    }
-
-
-    super({
-
-      jwtFromRequest:
-        ExtractJwt.fromAuthHeaderAsBearerToken(),
-
-      secretOrKey:
-        jwtSecret,
-
-    });
-
+  if (!jwtSecret) {
+    throw new Error("JWT_SECRET manquant");
   }
+
+  super({
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: jwtSecret,
+  });
+
+}
 
 
   async validate(payload:any){
