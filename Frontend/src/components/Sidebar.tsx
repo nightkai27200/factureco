12b50fcd08b5ce571fe61a414d2b31ac9a839a2f
nightkai27200 <kevdev59177@gmail.com>
2026-07-31 15:35:04 +0200

    feat: add home link to sidebar

--- Frontend/src/components/Sidebar.tsx
+++ Frontend/src/components/Sidebar.tsx
@@ -1,120 +1,113 @@
+
 "use client";
 
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 
-
-export default function Sidebar(){
-
+export default function Sidebar() {
   const router = useRouter();
 
-
-  function logout(){
-
+  function logout() {
     localStorage.removeItem("token");
-
     router.push("/login");
-
   }
 
-
   return (
-
     <aside
       style={{
-        width:"240px",
-        height:"100vh",
-        background:"#111827",
-        color:"white",
-        padding:"20px",
-        position:"fixed",
-        left:0,
-        top:0
+        width: "240px",
+        height: "100vh",
+        background: "#111827",
+        color: "white",
+        padding: "20px",
+        position: "fixed",
+        left: 0,
+        top: 0,
       }}
     >
-
-
-      
-
-
       <nav
         style={{
-          display:"flex",
-          flexDirection:"column",
-          gap:"15px",
-          marginTop:"30px"
+          display: "flex",
+          flexDirection: "column",
+          gap: "15px",
+          marginTop: "30px",
         }}
       >
 
+        {/* Retour à l'accueil */}
+        <Link
+          href="/"
+          style={{
+            color: "white",
+            textDecoration: "none",
+          }}
+        >
+          🏠 Accueil
+        </Link>
 
         <Link
           href="/dashboard"
-          style={{color:"white"}}
+          style={{
+            color: "white",
+            textDecoration: "none",
+          }}
         >
           Dashboard
         </Link>
 
-
-
         <Link
           href="/clients"
-          style={{color:"white"}}
+          style={{
+            color: "white",
+            textDecoration: "none",
+          }}
         >
           Clients
         </Link>
 
-
-
         <Link
-  href="/devis"
-  style={{
-    color:"white"
-  }}
->
-  Devis
-</Link>
-
-
+          href="/devis"
+          style={{
+            color: "white",
+            textDecoration: "none",
+          }}
+        >
+          Devis
+        </Link>
 
         <Link
           href="/invoices"
-          style={{color:"white"}}
+          style={{
+            color: "white",
+            textDecoration: "none",
+          }}
         >
           Factures
         </Link>
 
-
-
         <Link
           href="/company"
-          style={{color:"white"}}
+          style={{
+            color: "white",
+            textDecoration: "none",
+          }}
         >
           Société
         </Link>
-
-
       </nav>
 
-
-
       <button
-
         onClick={logout}
-
         style={{
-          marginTop:"40px",
-          padding:"10px",
-          width:"100%",
-          cursor:"pointer"
+          marginTop: "40px",
+          padding: "10px",
+          width: "100%",
+          cursor: "pointer",
         }}
-
       >
         Déconnexion
       </button>
-
-
     </aside>
-
   );
-
 }
+
